12bd03e5c604a23461d6ac5323f307de7bb0004b
nengcham <nengcham7@gmail.com>
2022-04-06 18:36:47 +0900

    user 수정

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,8 +1,13 @@
-// userRouter.post("/sign-up", (req, res)=>{
-//     const {id, password, name, phone} = req.body
-//     console.log(`넘어온 id 값 : ${id}`)
-//     console.log(`넘어온 password 값 : ${password}`)
-//     console.log(`넘어온 name 값 : ${name}`)
-//     console.log(`넘어온 phone 값 : ${phone}`)
-//     res.json(req.body)
-//   })
+const db = require('../models/index')
+const UserSchema = db.user
+
+exports.signup = (req, res) => {
+  new UserSchema({
+      username: req.body.username, 
+      password: req.body.password, 
+      name: req.body.name, 
+      telephone: req.body.telephone
+  }).save(()=>{
+      res.status(200).json({'result':'ok'}) 
+  })
+}
